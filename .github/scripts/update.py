20f44434605f097e32d4c4d43753854240750146
Paradoxdruid <bonham@gmail.com>
2021-07-24 17:39:36 -0600

    half implementation

--- .github/scripts/update.py
+++ .github/scripts/update.py
@@ -11,75 +11,81 @@ Examples:
 __version__ = "0.1"
 
 # imports
-from datetime import date
 import boto3
+import plotly.express as px
 import os
-import io
 from botocore.exceptions import ClientError
+from scholarly import scholarly
+from typing import Dict, Any
 
 
 # Amazon S3 constants
 BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-OBJECT_NAME = "scholar.csv"
+PROFILE_ID = "_FLoVbkAAAAJ&hl"
 
 
-def main(url, filename):
+def main(profile_id: str) -> None:
     """Wrapper to retrieve data, write updated csv, and use it to write updated graph
 
     Args:
-        url (str): url of Tableau dashboard to parse
-        filename (str): output csv filename
+        profile_id (str): google scholar author profile ID
     """
-    get_data_process_and_write_csv(url, filename)
+    get_data_process_and_write_files(profile_id)
 
 
-def get_data_process_and_write_csv(url, filename):
+def get_data_process_and_write_files(profile_id: str) -> None:
     """Wrapper to run all module functions.
 
     Args:
-        url (str): url of page with embedded Tableau display
-        filename (str): output .csv file to which we will append data
+        profile_id (str): google scholar author profile ID
     """
-    url_data = get_tableau_url(url)
-    data_url, data_dict = parse_tableau_config_return_data_url(url_data)
-    values = parse_tableau_data(data_url, data_dict)
-    values_with_date = prepend_date(values)
-    current_data = read_current_s3()
-    final_data = append_new_data(current_data, values_with_date)
-    save_to_s3(final_data)
+    scholar_data = get_scholar_data(profile_id)
+    fig = make_citation_figure(scholar_data)
+    metrics = make_citation_metrics(scholar_data)
+
+    save_to_s3("citations.png", fig)
+    save_to_s3("citations.csv", metrics)
 
 
-def read_current_s3():
-    """Retrieve latest covid data from Amazon s3 bucket and process as a csv fileobject.
+def get_scholar_data(profile_id: str) -> Dict[str, Any]:
+    author = scholarly.search_author_id(profile_id)
+    scholarly.fill(author, sections=["basics", "indices", "counts"])
 
-    Returns:
-        io.String: string object of covid data
-    """
-    s3_client = boto3.client("s3")
-    bytes_buffer = io.BytesIO()
-
-    s3_client.download_fileobj(
-        Bucket=BUCKET_NAME, Key=OBJECT_NAME, Fileobj=bytes_buffer
-    )
-    byte_value = bytes_buffer.getvalue()
-    str_value = byte_value.decode()
-    return io.StringIO(str_value)
+    return author
 
 
-def save_to_s3(final_data):
-    """Upload a file to an S3 bucket.
+def make_citation_figure(author: Dict[str, Any]) -> Any:
+    if isinstance(author.get("cites_per_year"), dict):
+        auth = author.get("cites_per_year")
+    else:
+        auth = {2021: 0}
+    fig = px.bar(x=auth.keys(), y=auth.values())
+    fig.update_xaxes(title="Year", dtick=1)
+    fig.update_yaxes(title="Citations per Year")
+
+    return fig
+
+
+def make_citation_metrics(author: Dict[str, Any]) -> None:
+    pass
+
+
+def save_to_s3(object_name: str, contents: str) -> None:
+    """Upload citation files to an S3 bucket.
 
     Args:
-        final_data: string fileobject of final data
+        object_name (str): name of object to store
+        contents: string fileobject of contents
 
     Returns:
         True if file was uploaded, else False
     """
 
     s3_client = boto3.client("s3")
+    # Citation Figure
     try:
         _ = s3_client.put_object(
-            Bucket=BUCKET_NAME, Key=OBJECT_NAME, Body=final_data.getvalue()
+            Bucket=BUCKET_NAME, Key=object_name, Body=contents.getvalue()
         )
     except ClientError as e:
         print(e)
@@ -87,19 +93,5 @@ def save_to_s3(final_data):
     return True
 
 
-def append_new_data(current_data, values):
-    """Write Tableau data values to a simple csv fileobject.
-
-    Args:
-        current_data: output fileobject
-        values (List[str]): list of Tableau data values
-    """
-    string_to_write = ",".join(str(v) for v in values) + "\n"
-    current_data.seek(0, 2)
-    current_data.write(string_to_write)
-    current_data.seek(0)
-    return current_data
-
-
 if __name__ == "__main__":
-    main(URL, FILENAME)
+    main(PROFILE_ID)
